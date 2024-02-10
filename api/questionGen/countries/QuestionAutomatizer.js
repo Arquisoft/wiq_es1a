756cb9e6b6ec61fb2f10fb8b334eec66eff1c5ef
CANCI0 <martinlol831@gmail.com>
2024-02-10 19:49:36 +0100

    Ya no se repiten respuestas

--- api/questionGen/countries/QuestionAutomatizer.js
+++ api/questionGen/countries/QuestionAutomatizer.js
@@ -3,22 +3,55 @@ const { format } = require('date-fns');
 const { es } = require('date-fns/locale');
 
 // q es la entidad, p la propiedad
-async function consultaSPARQL(entity, property) {
-    const endpointUrl = 'https://query.wikidata.org/sparql';
+async function consultaSPARQL(questionType, formatter = x => x) {
+    if(questionType.formatter){
+        formatter = questionType.formatter
+    }
 
-    const query = `
-        SELECT DISTINCT ?entity ?entityLabel ?propertyLabel WHERE {
-            ?entity wdt:P31 wd:${entity};
-                    wdt:${property} ?property.
-            FILTER NOT EXISTS {
-                ?entity wdt:${property} ?otherProperty.
-                FILTER (?propertyLabel != ?otherPropertyLabel)
-            }
+    const regex = /^Q\d+$/;
+
+
+    const queryTrue = `
+        SELECT ?entityLabel ?property ?propertyLabel
+        WHERE {
+            ?entity wdt:P31 wd:${questionType.entity};
+                wdt:${questionType.property} ?property .
             SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
         }
         ORDER BY UUID()
-        LIMIT 4          
+        LIMIT 1
     `;
+
+    var resultTrue = await executeQuery(queryTrue)
+
+    const entityLabel = resultTrue.results.bindings[0].entityLabel.value;
+    const propertyLabel = resultTrue.results.bindings[0].propertyLabel.value;
+    const url = resultTrue.results.bindings[0].property.type == 'url'? resultTrue.results.bindings[0].property.value : '';
+    const id = url.substring(url.lastIndexOf("/") + 1);
+
+    const queryFalse = `
+        SELECT DISTINCT ?propertyLabel
+        WHERE {
+        ?entity wdt:P31 wd:${questionType.entity};  # Selecciona entidades que son países (o cualquier entidad)
+            wdt:${questionType.property} ?property .
+            MINUS { ?property wdt:${questionType.property} wd:${id}. }  # Excluye entidades donde la propiedad dada es igual al valor dado
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es". }
+        }   
+        ORDER BY UUID()
+        LIMIT 3
+    `; 
+
+    var resultFalse = await executeQuery(queryFalse)
+
+    return {
+        pregunta: `${questionType.question} ${entityLabel}?`,
+        respuestas: [propertyLabel, ...resultFalse.results.bindings.map(b => b.propertyLabel.value)].map(x => formatter(x)),
+        respuestaCorrecta: formatter(propertyLabel)
+    };
+}
+
+async function executeQuery(query){
+    const endpointUrl = 'https://query.wikidata.org/sparql';
 
     const encodedQuery = encodeURIComponent(query);
     const sparqlUrl = `${endpointUrl}?query=${encodedQuery}&format=json`;
@@ -26,31 +59,7 @@ async function consultaSPARQL(entity, property) {
     const response = await fetch(sparqlUrl);
     const data = await response.json();
 
-    return data.results.bindings.map(binding => ({
-        entity: binding.entityLabel.value,
-        property: binding.propertyLabel.value
-    }));
-}
-
-async function obtenerPregunta(questionType, formatter = x => x) {
-    if(questionType.formatter){
-        formatter = questionType.formatter
-    }
-    const sparqlResult = await consultaSPARQL(questionType.entity, questionType.property);
-    const randomIndex = Math.floor(Math.random() * sparqlResult.length);
-    const selectedEntity = sparqlResult[randomIndex];
-
-    const pregunta = `${questionType.question} ${selectedEntity.entity}?`;
-    const respuestas = sparqlResult.map(result => result.property).map(x => formatter(x));
-    const respuestaCorrecta = formatter(selectedEntity.property);
-
-    const formato = {
-        pregunta: pregunta,
-        respuestas: respuestas,
-        respuestaCorrecta: respuestaCorrecta
-    };
-
-    return formato;
+    return data;
 }
 
 var questionTypes = [
@@ -59,13 +68,7 @@ var questionTypes = [
         "entity": "Q6256",       
         "property": "P36"
     },
-    {
-        "question": "¿Cuál fue la fecha de fundación de",
-        "entity": "Q6256",
-        "property": "P571",
-        "formatter": f => format(new Date(f), "dd 'de' MMMM 'de' yyyy", { locale: es })
-    },
-    {   //A veces se repiten los nombres
+    {   
         "question": "¿Quién es el jefe de estado de",
         "entity": "Q6256",       
         "property": "P35"
@@ -75,20 +78,24 @@ var questionTypes = [
         "entity": "Q6256",       
         "property": "P1344"
     },
-    {   //Más de una puede ser correcta
+    {   
         "question": "¿Cuál es uno de los idiomas oficiales de",
         "entity": "Q6256",       
         "property": "P37"
     },
-    {
-        "question": "¿Cuál es la esperanza de vida media de",
+    {   
+        "question": "¿Con qué país comparte frontera",
         "entity": "Q6256",       
-        "property": "P2250",
-        "formatter": x => Math.floor(x)
+        "property": "P47"
+    },
+    {   
+        "question": "¿Cuál es la moneda de",
+        "entity": "Q6256",       
+        "property": "P38"
     }
 ]
 
-// var randomType = questionTypes[Math.floor(Math.random() * questionTypes.length)];
-var randomType = questionTypes[4];
+//var randomType = questionTypes[Math.floor(Math.random() * questionTypes.length)];
+var randomType = questionTypes[2];
 
-obtenerPregunta(randomType).then(pregunta => console.log(pregunta));
+consultaSPARQL(randomType).then(pregunta => console.log(pregunta));
