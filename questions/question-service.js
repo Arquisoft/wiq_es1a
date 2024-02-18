c40363f6acee41f51b2156dfbcf2998b4d242523
CANCI0 <martinlol831@gmail.com>
2024-02-18 11:35:33 +0100

    Añadido límite de preguntas por petición

--- questions/question-service.js
+++ questions/question-service.js
@@ -8,20 +8,25 @@ const app = express();
 const port = 8003;
 
 const gen = new GeneratorChooser();
+const MAX_QUESTIONS = 100;
 
 // Middleware to parse JSON in request body
 app.use(bodyParser.json());
 
+app.set('json spaces', 40);
+
 app.get('/randomQuestion', async (req, res) => {
-    var inicio = performance.now();
+    if(req.query.n > MAX_QUESTIONS){
+      res.status(400).json({ error: `El límite de preguntas son ${MAX_QUESTIONS}`}); 
+    }
     try {       
         var data = await gen.getCountryQuestions(req.query.n);
         res.json(data);
     } catch (error) {
         res.status(400).json({ error: error.message }); 
+    }
     var fin  = performance.now();
-    console.log("Tiempo transcurrido en milisegundos: " + (fin - inicio));
-}});
+});
 
 const server = app.listen(port, () => {
   console.log(`Question Service listening at http://localhost:${port}`);
