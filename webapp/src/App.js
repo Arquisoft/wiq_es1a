8e861fc71a55dccf72c3893e666bef5ee96b693f
CANCI0 <martinlol831@gmail.com>
2024-02-20 19:30:02 +0100

    Interfaz básica

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,37 +1,26 @@
 import React, { useState } from 'react';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import Authenticate from './pages/Authenticate/Authenticate.js';
+import Home from './pages/Home/Home.js';
+import Clasico from './pages/Clasico/Clasico.js';
+import WrongRoute from './pages/WrongRoute/WrongRoute.js';
+import './App.css';
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [showLogin, setShowLogin] = useState(true);
-
-  const handleToggleView = () => {
-    setShowLogin(!showLogin);
-  };
-
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
-        Welcome to wiq_es1a
-      </Typography>
-      {showLogin ? <Login /> : <Register />}
-      <Typography component="div" align="center" sx={{ marginTop: 2 }}>
-        {showLogin ? (
-          <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
-            Don't have an account? Register here.
-          </Link>
-        ) : (
-          <Link component="button" variant="body2" onClick={handleToggleView}>
-            Already have an account? Login here.
-          </Link>
-        )}
-      </Typography>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        {/** Rutas públicas */}
+        <Route path={'/'} element={<Authenticate />}/>
+
+        {/** Rutas privadas */}
+        <Route path='/home' element={<Home />} />
+        <Route path='/home/clasico' element={<Clasico />} />
+
+        <Route path='*' element={<WrongRoute />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
