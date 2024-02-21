88bdb06024ab445073597c505ed05bd79ce30d75
rodrox11 <rodrigsporting1104@gmail.com>
2024-02-21 21:44:10 +0100

    Autentificación de usuario

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,19 +5,30 @@ import Clasico from './pages/Clasico/Clasico.js';
 import WrongRoute from './pages/WrongRoute/WrongRoute.js';
 import './App.css';
 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+//Autentificamos que el usuario este registrado en la aplicación
+const [authenticated, setAuthenticated] = useState(false);
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/** Rutas públicas */}
-        <Route path={'/'} element={<Authenticate />}/>
+        <Route path='/' element={<Authenticate setAuthenticated={setAuthenticated} />} />
 
+        /**Rutas que estan protegidas si no estas registrado */
         {/** Rutas privadas */}
-        <Route path='/home' element={<Home />} />
-        <Route path='/home/clasico' element={<Clasico />} />
+        {authenticated ? (
+          <>
+            <Route path='/home' element={<Home />} />
+            <Route path='/home/clasico' element={<Clasico />} />
+          </>
+        ) : (
+          <Navigate to="/" />
+        )}
 
+        {/* Ruta por defecto */}
         <Route path='*' element={<WrongRoute />} />
       </Routes>
     </BrowserRouter>
