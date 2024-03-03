a81d4bbe57a496e01083c7dcd634ce3ec86a9672
CANCI0 <martinlol831@gmail.com>
2024-03-03 12:47:23 +0100

    Añadadida pregunta de banderas de países

--- questionservice/questionGen/GenericGenerator.js
+++ questionservice/questionGen/GenericGenerator.js
@@ -146,6 +146,10 @@ class GenericGenerator {
         questionObj.respuestas = questionObj.respuestas.map(x => Math.floor(x));
         questionObj.correcta = Math.floor(questionObj.correcta);
         break;
+      case "photo":
+        questionObj.respuestas = questionObj.respuestas.map(x => this.#photoFormatter(x));
+        questionObj.correcta = this.#photoFormatter(questionObj.correcta);
+        break;
       default:
         break;
     }
@@ -179,7 +183,11 @@ class GenericGenerator {
     const fechaFormateada = `${dia}/${mes}/${año}${isAC ? ' a.C.' : ''}`;
     
     return fechaFormateada;
-}
+  }
+
+  #photoFormatter(photo){
+    return `<a href="${photo}" ></a>`;
+  }
 
 }
 
