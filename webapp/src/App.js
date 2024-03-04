69103482cea8fb4ab7c36d68f02de53297ea1a05
CANCI0 <martinlol831@gmail.com>
2024-03-04 19:40:29 +0100

    Fixes

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -7,21 +7,9 @@ import WrongRoute from './pages/WrongRoute/WrongRoute.js';
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-
 //Autentificamos que el usuario este registrado en la aplicación
 //const [authenticated, setAuthenticated] = useState(false);
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/** Rutas públicas */}
-        <Route path='/' element={<Authenticate />} />
-
-        {/** Rutas privadas */}
-        <Route path='/home' element={<Home />} />
-        <Route path='/home/clasico' element={<Clasico />} />
-
   return (
     <BrowserRouter>
       <Routes>
