b2a955808ce9e0ea12a50c924e803ad4988ee9c6
UO288443 <uo288443@uniovi.es>
2024-03-05 10:43:46 +0100

    Test WrongRute

---
+++ webapp/src/pages/WrongRoute/WrongRute.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import WrongRoute from './WrongRoute';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+test('renders 404 message', () => {
+  render(
+    <Router>
+      <WrongRoute />
+    </Router>
+  );
+  const errorMessage = screen.getByText(/404/i);
+  expect(errorMessage).toBeInTheDocument();
+});
+
+test('renders "Página no encontrada" message', () => {
+  render(
+    <Router>
+      <WrongRoute />
+    </Router>
+  );
+  const notFoundMessage = screen.getByText(/Página no encontrada/i);
+  expect(notFoundMessage).toBeInTheDocument();
+});
+
+test('renders link to home page', () => {
+  render(
+    <Router>
+      <WrongRoute />
+    </Router>
+  );
+  const homeLink = screen.getByRole('link', { name: /página principal/i });
+  expect(homeLink).toHaveAttribute('href', '/home');
+});
