b5949d03eaa38f99ada191a3f71efb48589d3781
UO288443 <UO288443@uniovi.es>
2024-03-09 15:20:34 +0100

    Modo de juego conoce ciudades

---
+++ webapp/src/pages/ConoceCiudades/ConoceCiudades.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+import "./Clasico.css";
+import Preguntas from "../../components/PreguntasConoceCiudades";
+import { useNavigate } from "react-router-dom";
+import Nav from '../../components/Nav/Nav.js';
+import { Link } from 'react-router-dom';
+import Footer from "../../components/Footer/Footer.js";
+
+const JuegoPreguntas = () => {
+  const [indicePregunta, setIndicePregunta] = useState(0);
+  const [puntuacion, setPuntuacion] = useState(0);
+  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState(null);
+  const [tiempoRestante, setTiempoRestante] = useState(10);
+  const [juegoTerminado, setJuegoTerminado] = useState(false);
+  const [mostrarMenu, setMostrarMenu] = useState(false); // Estado para mostrar el menú al finalizar el juego
+  const preguntaActual = Preguntas[indicePregunta];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (tiempoRestante === 0) {
+      setTimeout(() => {
+        handleSiguientePregunta();
+      }, 3000);
+    }
+    const timer = setInterval(() => {
+      setTiempoRestante((prevTiempo) => (prevTiempo <= 0 ? 0 : prevTiempo - 1));
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [tiempoRestante, indicePregunta]);
+
+  useEffect(() => {
+    if (juegoTerminado) {
+      setMostrarMenu(true);
+    }
+  }, [juegoTerminado]);
+
+  const handleRespuestaSeleccionada = (respuesta) => {
+    if (!juegoTerminado) {
+      setRespuestaSeleccionada(respuesta);
+    }
+  };
+
+  const estiloRespuesta = (respuesta) => {
+    if (juegoTerminado) {
+      if (respuesta === preguntaActual.respuestaCorrecta) {
+        return { backgroundColor: "green" };
+      } else if (respuesta === respuestaSeleccionada) {
+        return { backgroundColor: "red" };
+      }
+    } else {
+      if (respuesta === respuestaSeleccionada) {
+        return { backgroundColor: "#0F0F0F", color: "#F0F0F0" };
+      }
+    }
+    return {};
+  };
+
+  const handleSiguientePregunta = () => {
+    if (respuestaSeleccionada === preguntaActual.respuestaCorrecta) {
+      setPuntuacion(puntuacion + 1);
+    }
+    setRespuestaSeleccionada(null);
+    setTiempoRestante(10);
+    if (indicePregunta + 1 < Preguntas.length) {
+      setIndicePregunta(indicePregunta + 1);
+    } else {
+      setJuegoTerminado(true);
+    }
+  };
+
+  const handleRepetirJuego = () => {
+    // Reiniciar el estado para repetir el juego
+    setIndicePregunta(0);
+    setPuntuacion(0);
+    setRespuestaSeleccionada(null);
+    setTiempoRestante(10);
+    setJuegoTerminado(false);
+    setMostrarMenu(false);
+  };
+
+  if (mostrarMenu) {
+    return (
+      <>
+        <Nav />
+        <div className="menuContainer">
+          <h2>¡Juego terminado!</h2>
+          <p>
+            Tu puntuación: {puntuacion}/{Preguntas.length}
+          </p>
+          <button onClick={handleRepetirJuego}>Repetir Juego</button>
+          <Link to="/home">Volver al Menú Principal</Link>
+        </div>
+        <Footer />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Nav />
+      <div className="questionContainer">
+        <h2>Pregunta {indicePregunta + 1}:</h2>
+        <p>{preguntaActual.pregunta}</p>
+        <img src={preguntaActual.imgsrc} alt="MDN" />
+        <div className="responsesContainer">
+          {preguntaActual.respuestas.map((respuesta, index) => (
+            <button
+              key={index}
+              onClick={() => handleRespuestaSeleccionada(respuesta)}
+              disabled={tiempoRestante === 0 || juegoTerminado}
+              style={estiloRespuesta(respuesta)}
+            >
+              {respuesta}
+            </button>
+          ))}
+        </div>
+        <div className="timer">Tiempo restante: {tiempoRestante}</div>
+        <div className="points">Puntuación: {puntuacion}</div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default JuegoPreguntas;
