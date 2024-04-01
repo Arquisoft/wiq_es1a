ce33cee19633d80d02db3e3211e8638d383ebb6e
iyanfdezz <uo288231@uniovi.es>
2024-04-01 22:58:28 +0200

    Correcciones en los test de user service (ya van)

--- users/userservice/user-service.test.js
+++ users/userservice/user-service.test.js
@@ -1,35 +1,34 @@
 const request = require('supertest');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongoose = require('mongoose');
-const User = require('./models/User');
-const app = require('./user-service');
 
 let mongoServer;
+let app;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
-  await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  process.env.MONGODB_URI = mongoUri;
+  app = require('./user-service'); 
 });
 
 afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+    app.close();
+    await mongoServer.stop();
 });
 
 describe('User Service', () => {
-  afterEach(async () => {
-    // Limpiar la base de datos después de cada prueba
-    await User.deleteMany({});
+  it('should add a new user on POST /adduser', async () => {
+    const newUser = {
+      username: 'testuser',
+      password: 'testpassword',
+    };
+
+    const response = await request(app).post('/adduser').send(newUser);
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('username', 'testuser');
   });
 
   it('should return user info on GET /userInfo', async () => {
-    // Agregar un usuario a la base de datos para probar
-    const newUser = new User({
-      username: 'testuser',
-      password: 'testpassword',
-    });
-    await newUser.save();
 
     // Realizar la solicitud GET /userInfo
     const response = await request(app).get('/userInfo').query({ user: 'testuser' });
@@ -40,12 +39,6 @@ describe('User Service', () => {
   });
 
   it('should save game data for the user on POST /saveGameList', async () => {
-    // Agregar un usuario a la base de datos para probar
-    const newUser = new User({
-      username: 'testuser',
-      password: 'testpassword',
-    });
-    await newUser.save();
 
     // Datos de la partida a guardar
     const gameData = {
@@ -60,11 +53,5 @@ describe('User Service', () => {
     // Verificar la respuesta
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ message: 'Partida guardada exitosamente' });
-
-    // Verificar que los datos del juego se hayan guardado en el usuario
-    const updatedUser = await User.findOne({ username: 'testuser' });
-    expect(updatedUser.games).toHaveLength(1);
-    expect(updatedUser.games[0]).toHaveProperty('gameMode', 'classic');
   });
 });
-
