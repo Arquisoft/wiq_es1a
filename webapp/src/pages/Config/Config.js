dff741e527e94afe919f6f21312a78bbf57f503b
CANCI0 <martinlol831@gmail.com>
2024-04-05 21:01:10 +0200

    Creada estructura para la internacionalización

--- webapp/src/pages/Config/Config.js
+++ webapp/src/pages/Config/Config.js
@@ -12,15 +12,20 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Select,
 } from "@chakra-ui/react";
 import Nav from "../../components/Nav/Nav.js";
 import Footer from "../../components/Footer/Footer.js";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Config = () => {
+  const { i18n } = useTranslation();
+
   useEffect(() => {
     // Obtener el estado de los checkboxes desde el localStorage
-    const selectedThemes = JSON.parse(localStorage.getItem("selectedThemes")) || [];
+    const selectedThemes =
+      JSON.parse(localStorage.getItem("selectedThemes")) || [];
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     checkboxes.forEach((checkbox) => {
@@ -86,6 +91,15 @@ const Config = () => {
           <Heading as="h2" mb={4}>
             Configuración
           </Heading>
+          <FormLabel htmlFor="idioma"> Idioma</FormLabel>
+          <Select id="idioma" mb="1rem">
+            <option onClick={() => i18n.changeLanguage("es")}>
+              Español
+            </option>
+            <option onClick={() => i18n.changeLanguage("en")}>
+              English
+            </option>
+          </Select>
           <FormLabel htmlFor="clasico"> Temáticas de preguntas</FormLabel>
           <Box
             display="grid"
