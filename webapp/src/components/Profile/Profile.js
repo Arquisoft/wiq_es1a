b3ba3f2b791108101ef6f4a2d00c302b7f73e7a9
CANCI0 <martinlol831@gmail.com>
2024-04-06 20:54:12 +0200

    Ahora se muestran los perfiles de los amigos

---
+++ webapp/src/components/Profile/Profile.js
@@ -0,0 +1,94 @@
+import { Box, VStack, Heading, Text, Center, Spinner, Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+const Perfil = (username) => {
+  const gatewayUrl = process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000";
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(gatewayUrl + `/userInfo?user=${username.username}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Perfil del usuario:', data);
+        setUserData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message || 'Ha ocurrido un error al obtener el perfil');
+        setLoading(false);
+      });
+      // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <Center py={8}>
+        <Box w="xl" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="lg" width="100%">
+          <VStack p={8} align="start" spacing={6}>
+            <Heading as="h1" size="lg">
+              Perfil del usuario
+            </Heading>
+            {loading ? (
+              <Center>
+                <Spinner />
+              </Center>
+            ) : error ? (
+              <Text>Error: {error}</Text>
+            ) : (
+              <>
+                {userData && (
+                  <>
+                    <Text>
+                      <strong>Nombre de usuario:</strong> {userData.username}
+                    </Text>
+                    <Text>
+                      <strong>Fecha de creación de la cuenta:</strong>{" "}
+                      {new Date(userData.createdAt).toLocaleString()}
+                    </Text>
+                    <Heading as="h2" size="md">
+                      Partidas Recientes
+                    </Heading>
+                    <div style={{ width: '100%'}}>
+                      {userData.games.length > 0 ? (
+                        <Table variant="simple">
+                          <Thead>
+                            <Tr>
+                              <Th>Modo de juego</Th>
+                              <Th>Respuestas correctas</Th>
+                              <Th>Respuestas incorrectas</Th>
+                              <Th>Puntos</Th>
+                              <Th>Tiempo promedio</Th>
+                            </Tr>
+                          </Thead>
+                          <Tbody>
+                            {userData.games.slice(0, 10).map((game, index) => (
+                            <Tr key={index}>
+                              <Td>{game.gamemode}</Td>
+                              <Td>{game.gamemode === 'calculadora' ? '-' : game.correctAnswers}</Td>
+                              <Td>{game.gamemode === 'calculadora' ? '-' : game.incorrectAnswers}</Td>
+                              <Td>{game.points}</Td>
+                              <Td>{parseFloat(game.avgTime).toFixed(2)} segundos</Td>
+                            </Tr>
+                            ))}
+                          </Tbody>
+                        </Table>
+                      ) : (
+                        <Text>No hay partidas recientes.</Text>
+                      )}
+                    </div>
+                  </>
+                )}
+              </>
+            )}
+          </VStack>
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default Perfil;
+
--- webapp/src/pages/Social/FriendsList.js
+++ webapp/src/pages/Social/FriendsList.js
@@ -1,71 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Container, Text, List, ListItem, Divider, Heading } from '@chakra-ui/react';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Text,
+  List,
+  ListItem,
+  Divider,
+  Heading,
+  Button
+} from "@chakra-ui/react";
 import Nav from "../../components/Nav/Nav.js";
 import Footer from "../../components/Footer/Footer.js";
+import Profile from "../../components/Profile/Profile.js";
 
 const FriendList = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [friends, setFriends] = useState([]);
-    const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+  const [isLoading, setIsLoading] = useState(false);
+  const [friends, setFriends] = useState([]);
+  const [friend, setFriend] = useState("");
+  const apiEndpoint =
+    process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000";
 
-    const fetchFriends = async () => {
-        const username = localStorage.getItem('username');
-        setIsLoading(true);
-        fetch(`${apiEndpoint}/friends?user=${username}`)
-        .then((response) => response.json())
-        .then((data) => {
-            setFriends(data.friends);
-            setIsLoading(false);
-        })
-        .catch((error) => {
-            console.error("Error al obtener los amigos:", error);
-            setIsLoading(false);
-        });
-    };
+  const fetchFriends = async () => {
+    const username = localStorage.getItem("username");
+    setIsLoading(true);
+    fetch(`${apiEndpoint}/friends?user=${username}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFriends(data.friends);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los amigos:", error);
+        setIsLoading(false);
+      });
+  };
 
-    useEffect(() => {
-        fetchFriends();
-    }, []);
+  useEffect(() => {
+    fetchFriends();
+  }, []);
 
-    if (isLoading) {
-        return (
-          <div>
-            <Heading as="h2"> Cargando ... </Heading>
-            <p>Se está consultando su búsqueda, espere unos instantes.</p>
-          </div>
-        );
-      }
-
+  if (isLoading) {
     return (
-        <>
-        <Nav/>
-        <Container maxW="md">
-            <Text fontSize="3xl" textAlign="center" mb="4">
-                Lista de Amigos
-            </Text>
-            {friends.length > 0 ? (
-                <List spacing={3}>
-                    {friends.map((friend, index) => (
-                        <div key={friend._id}>
-                            <ListItem>
-                                <Text>{friend}</Text>
-                            </ListItem>
-                            {index !== friends.length - 1 && <Divider />}
-                        </div>
-                    ))}
-                </List>
-            ) : (
-                <Text fontSize="xl" textAlign="center">
-                    No tienes amigos actualmente.
-                </Text>
-            )}
-        </Container>
-        <Footer/>
-        </>  
-        
+      <div>
+        <Heading as="h2"> Cargando ... </Heading>
+        <p>Se está consultando su búsqueda, espere unos instantes.</p>
+      </div>
     );
+  }
+
+  return (
+    <>
+      <Nav />
+      {friend ? (
+        <>
+            <Profile username={friend}/>
+            <Button onClick={() => setFriend('')}>Volver</Button>
+        </>
+      ) : (
+        <Container maxW="md">
+          <Text fontSize="3xl" textAlign="center" mb="4">
+            Lista de Amigos
+          </Text>
+          {friends.length > 0 ? (
+            <List spacing={3}>
+              {friends.map((friend, index) => (
+                <div key={friend._id}>
+                  <ListItem>
+                    <Text onClick={() => setFriend(friend)}>{friend}</Text>
+                  </ListItem>
+                  {index !== friends.length - 1 && <Divider />}
+                </div>
+              ))}
+            </List>
+          ) : (
+            <Text fontSize="xl" textAlign="center">
+              No tienes amigos actualmente.
+            </Text>
+          )}
+        </Container>
+      )}
+      <Footer />
+    </>
+  );
 };
 
 export default FriendList;
-
