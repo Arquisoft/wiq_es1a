95543fe8b6e9a1de2e2f09fcae81cd80d3ef964f
rodrox11 <rodrigsporting1104@gmail.com>
2024-04-26 22:44:49 +0200

    pagina central de ayuda

---
+++ webapp/src/pages/Ayuda/Ayuda.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Heading, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import Nav from '../../components/Nav/Nav';
+import Footer from '../../components/Footer/Footer';
+
+const Ayuda = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Nav />
+      <Heading as="h1">{t('pages.help.title')}</Heading>
+      <img src = "/helCenter.jpeg"> alt={t('pages.help.title')}</img>
+      <Heading as="h2" size="md">
+        {t('pages.help.description')}
+      </Heading>
+      <Table className="help-table" id="help-table">
+        <Thead>
+          <Tr>
+            <Th>{t('pages.help.category')}</Th>
+            <Th>{t('pages.help.descriptionC')}</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>{t('pages.help.gameModes')}</Td>
+            <Td>
+              {t('pages.help.gameModesDescription')}{' '}
+              <a href="/ayuda/modos-de-juego">{t('pages.help.moreDetails')}</a>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>{t('pages.help.socialHelp')}</Td>
+            <Td>
+              {t('pages.help.socialHelpDescription')}{' '}
+              <a href="/ayuda/social">{t('pages.help.moreDetails')}</a>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>{t('pages.help.statsHelp')}</Td>
+            <Td>
+              {t('pages.help.statsHelpDescription')}{' '}
+              <a href="/ayuda/estadisticas">{t('pages.help.moreDetails')}</a>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
+      <Footer />
+    </>
+  );
+};
+
+export default Ayuda;
