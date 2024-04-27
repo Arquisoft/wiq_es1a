bfdd47fc92cb39c3e099f92613cb29770f173d5f
iyanfdezz <uo288231@uniovi.es>
2024-04-27 12:05:25 +0200

    Arreglados tests

--- webapp/src/pages/Clasico/Clasico.js
+++ webapp/src/pages/Clasico/Clasico.js
@@ -313,7 +313,7 @@ const JuegoPreguntas = () => {
                 <p>
                   {t("pages.classic.time")} {Math.floor(timeLeft)}
                 </p>
-                <p>{t("pages.classic.score")} {puntuacion}</p>
+                <p>{t("pages.classic.score")} {points}</p>
                 <Box w="100%" bg="gray.100" borderRadius="lg" mt={4}>
                   <Box
                     bg="teal.500"
